e5f9751d2cc76ed21461bc156e3360aa3e886747
Yongkang Zhao <cookieyong@gmail.com>
2020-11-01 14:07:20 -0800

    tried all kinds of primitive and reference data types

--- app.js
+++ app.js
@@ -35,22 +35,63 @@
 
 // // ##CONST
 
-const name = 'John';
-// cannot reassign
-// name = 'Sara';
-// Have to assign a value
-// const greeting;
-const person = {
-    name: 'John',
-    age: 30
+// const name = 'John';
+// // cannot reassign
+// // name = 'Sara';
+// // Have to assign a value
+// // const greeting;
+// const person = {
+//     name: 'John',
+//     age: 30
+// }
+
+// person.name = 'Sara';
+// person.age = 32;
+
+// console.log(person);
+
+// const numbers = [1,2,3,4,5];
+// numbers.push(6);
+
+// console.log(numbers);
+
+
+
+// Primitive Data Types
+// String
+const name = 'John Doe';
+console.log(typeof name);
+// Number
+const age = 45;
+console.log(typeof age);
+// Boolean
+const hasKids = true;
+console.log(typeof hasKids);
+// Null
+const car = null;
+console.log(typeof car);
+// Undefined
+let test;
+console.log(typeof test);
+// Symbols
+const sym = Symbol();
+console.log(typeof sym);
+
+
+// Reference Data types/ objects
+// Arrays
+const hobbies = ['movies','music'];
+console.log(typeof hobbies);
+// Object 
+const address = {
+    city: 'Boston',
+    state: 'MA'
 }
+console.log(typeof address);
 
-person.name = 'Sara';
-person.age = 32;
+// Functions
+// Dates
+const today = new Date();
+console.log(typeof today);
+// Anything else....
 
-console.log(person);
-
-const numbers = [1,2,3,4,5];
-numbers.push(6);
-
-console.log(numbers);
