f5a3769907275d2ce52f02ac02cd151bc082ab7a
Yongkang Zhao <cookieyong@gmail.com>
2020-11-04 21:56:08 -0800

    played with single element query selector

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-let val;
+// // document.getElementById()
 
-val = document;
+// console.log(document.getElementById("task-title"));
 
-document.head;
+// // get things from th element
 
-console.log(val.all);
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
+
+// // change styling
+
+// document.getElementById('task-title').style.background = '#999';
+// document.getElementById('task-title').style.color = '#fff';
+// document.getElementById('task-title').style.padding = '5px';
+
+// // change content
+// document.getElementById('task-title').textContent = 'Task List';
+// document.getElementById('task-title').innerText = "My Tasks";
+// document.getElementById('task-title').innerHTML = '<span style="color:red">Task List</span>';
+
+// document.querySelector()
+
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
+
+// document.querySelector('div').style.color='red';
+// document.querySelector('body div:last-child').style.color='red';
