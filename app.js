b65c26d026b30e90b123262213aa7a15afecdfc0
Yongkang Zhao <cookieyong@gmail.com>
2020-11-04 21:39:47 -0800

    starting to learn about the DOM

--- app.js
+++ app.js
@@ -1,31 +1,7 @@
-const name = 'John';
-const age = 123;
-const job = 'Web Dev';
-const city = 'Miami';
-let html;
-//Without template strings;
+let val;
 
-html = "<ul><li>Name: " + name + "</li><li>Age: " + age + "</li><li>Job: " + job + "</li><li>City: " + city + "</li></ul>";
+val = document;
 
-html = "<ul>"+
-        "<li>Name: " + name + "</li>"+
-        "<li>Age: " + age + "</li>"+
-        "<li>Job: " + job + "</li>"+
-        "<li>City: " + city + "</li>"+
-        "</ul>";
+document.head;
 
-//With template strings;
-
-html = `
-    <ul>
-    <li>Name: ${name}</li>
-    <li>Age: ${age}</li>
-    <li>Job: ${job}</li>
-    <li>City: ${city}</li>
-    </ul>
-
-
-
-`;
-
-document.body.innerHTML = html;
+console.log(val.all);
