f25dfbbe80570e291d832c71b88fa99fb025b8e5
Yongkang Zhao <cookieyong@gmail.com>
2020-11-05 19:41:32 -0800

    played with some more  DOM manipulation methods

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
-// create element
-const li = document.createElement('li');
-
-// add class
-li.className = 'collection-item';
-
-// add id
-li.id = 'new-item';
-
-// add attribute
-li.setAttribute('title', 'New Item');
-
-// Create text node and append
-li.appendChild(document.createTextNode('Hello World'));
-
-// create new link element
-const link = document.createElement('a');
-link.className = 'delete-item secondary-content';
-link.innerHTML = '<i class = "fa fa-remove"></i>';
-li.appendChild(link);
-
-// append li as child to ul
-document.querySelector('ul.collection').appendChild(li);
-
-console.log(li);
+document.querySelector('.clear-tasks').addEventListener('click',
+function(e){
+    console.log('Hello World');
+    e.preventDefault();
+});
