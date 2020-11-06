b88eb8d9f79d1ec9a711def84a52992345f0da61
Yongkang Zhao <cookieyong@gmail.com>
2020-11-05 20:32:33 -0800

    played with some more  DOM manipulation methods

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
-document.querySelector('.clear-tasks').addEventListener('click',
+// set local storage item
+// localStorage.setItem('name', 'John');
+
+// // set session storage item
+// // sessionStorage.setItem('name', 'Beth');
+
+
+// let name = localStorage.getItem('name');
+// console.log(name);
+
+// localStorage.clear();
+// name = localStorage.getItem('name');
+// console.log(name);
+
+
+document.querySelector('form').addEventListener('submit',
 function(e){
-    console.log('Hello World');
+    let task = document.getElementById('task').value;
+
+    let tasks;
+
+    if (localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    console.log(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    // const newTask = document.createElement('li');
+    // newTask
     e.preventDefault();
-});
+}
+);
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+console.log(tasks);
+for (const task in tasks){
+    console.log(tasks[task]);
+}
