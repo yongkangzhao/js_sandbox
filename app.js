aa6c044c3ef304887410f1358a7a39a4f38fea95
Yongkang Zhao <cookieyong@gmail.com>
2020-11-06 16:34:28 -0800

    played with class objects

--- app.js
+++ app.js
@@ -1,42 +1,13 @@
-// set local storage item
-// localStorage.setItem('name', 'John');
+// Person constructor
 
-// // set session storage item
-// // sessionStorage.setItem('name', 'Beth');
-
-
-// let name = localStorage.getItem('name');
-// console.log(name);
-
-// localStorage.clear();
-// name = localStorage.getItem('name');
-// console.log(name);
-
-
-document.querySelector('form').addEventListener('submit',
-function(e){
-    let task = document.getElementById('task').value;
-
-    let tasks;
-
-    if (localStorage.getItem('tasks') === null){
-        tasks = [];
-    }else{
-        tasks = JSON.parse(localStorage.getItem('tasks'));
+class Person {
+    constructor(name) {
+        this.name = name;
     }
-
-    tasks.push(task);
-
-    console.log(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    // const newTask = document.createElement('li');
-    // newTask
-    e.preventDefault();
 }
-);
 
-const tasks = JSON.parse(localStorage.getItem('tasks'));
-console.log(tasks);
-for (const task in tasks){
-    console.log(tasks[task]);
-}
+
+const brad = new Person('brad');
+
+
+console.log(brad);
